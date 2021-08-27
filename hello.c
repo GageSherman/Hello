7a6ffc84dc2ef6fe22e3bc823eb099ceed449072
Gage <gsherman3@huskers.unl.edu>
2021-08-27 00:00:05 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,16 +1,17 @@
 /**
  * Author: Gage Sherman
- * Date: 2021/08/24
+ * Date: 2021/08/26
  *
  * A simple hello world program in C
  *
  */
 #include <stdlib.h>
 #include <stdio.h>
- 
+
   int main(int argc, char **argv) {
 
-     printf("Gage Sherman\n");
+     printf("Gage Sherman!\n");
+     printf("Computer Science\n");
 
      return 0;
  }
